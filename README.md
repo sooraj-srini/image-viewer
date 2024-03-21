aa1a9024b9924a157c111e4ed58d91dc443d7b4e
sooraj-srini <cs20b075@smail.iitm.ac.in>
2024-03-21 12:52:34 +0530

    rgb channelsgit add .git add .!

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This is an image viewer. I initially wanted to make a png decoder as well, but I just gave up. smh. 
 
+# Features
+
+- Open exactly one image file ('./japan.png')
+- Can open other images if they have the same path
+- Can toggle between rgb channels!!!!!
+- fixed at 60fps
--- src/main.rs
+++ src/main.rs
@@ -30,18 +30,34 @@ fn main() {
 
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
+    let mut count = 0;
+    
+    //find time window is open 
+    let start_time = std::time::Instant::now();
+    
+    buffer.iter_mut().enumerate().for_each(|(i, x)| { *x = ((buf[i][0] as u32) << 16 | (buf[i][1] as u32)<<8 | buf[i][2] as u32) as u32});
+    
     while window.is_open() && !window.is_key_down(Key::Enter) {
-        // for i in buffer.iter_mut() {
-        //     *i = buf[i][0]*256*256 + buf[i][1]*256 + buf[i][2]; // write something more funny here!
-        //     // let r = buf[0][0];
-        // }
+        
+        if window.is_key_pressed(Key::R, minifb::KeyRepeat::No){
+            buffer.iter_mut().enumerate().for_each(|(i, x)| {*x = ((buf[i][0] as u32)<<16)^(*x)});
+        }
+        if window.is_key_pressed(Key::G, minifb::KeyRepeat::No){
+            buffer.iter_mut().enumerate().for_each(|(i, x)| {*x = ((buf[i][1] as u32)<<8)^(*x)});
+        }
+        if window.is_key_pressed(Key::B, minifb::KeyRepeat::No){
+            buffer.iter_mut().enumerate().for_each(|(i, x)| {*x = ((buf[i][2] as u32))^(*x)});
+        }
 
-        buffer.iter_mut().enumerate().for_each(|(i, x)| { *x = ((buf[i][0] as u32) << 16 | (buf[i][1] as u32)<<8 | buf[i][2] as u32) as u32});
-
+        count = count + 1;
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
             .update_with_buffer(&buffer, width as usize, height as usize)
             .unwrap();
     }
+    let end_time = std::time::Instant::now();
+    let diff = end_time - start_time;
+    println!("{count}");
+    println!("{:?}", diff);
+    println!("Frame rate: {}", count as f64 / diff.as_secs_f64());
 }
